60e25766fcc592d66ff82a8d2582d502fe9698e2
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-02-24 16:30:38 +0000

    add data type file

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,16 @@
+let userName='adarsh'   //string
+
+let age=49  //numbers
+
+let loggin = false  //boolean
+
+// bigInt => for number very big numbers
+// null => stand alone value
+// undefined => value of var is not assign
+// symbols => uniqe
+
+// console.log(typeof userName);
+
+console.table([typeof userName, typeof age, typeof loggin, typeof null, typeof undefined])
+
+// null is shown as object
