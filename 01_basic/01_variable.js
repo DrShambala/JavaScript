b7017e40f4c611152d5c2a6f9be3345ff327f789
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-02-24 15:54:06 +0000

    add variable file

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+//VARIABLES
+
+const accId=12345   // it can not be change
+
+let email= 'example@gmail.com'  // does not have block issue
+
+var passwd= 'eample123' // does have block issue
+
+city='umbrella' // not a good way of declaring variable
+
+let state;      // an variable whose value is not defined
+// it is not always necessary to use semi colon
+
+console.log(accId);
+
+// accId=2
+// console.log(accId);
+
+console.table([accId,email,passwd,city,state])
