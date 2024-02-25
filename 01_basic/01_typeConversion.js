55937b971ab7a75fc984962bd56f3946c42e8103
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-02-25 06:34:59 +0000

    added operations

--- 01_basic/01_typeConversion.js
+++ 01_basic/01_typeConversion.js
@@ -43,3 +43,35 @@ console.table([bool,typeof bool, bool2,typeof bool2, bool3, typeof bool3])
  'true'    
  'string'  
 */
+
+
+//============================ O P E R A T I O N S =========================================
+
+let val= 10
+let negVal = -val
+console.log(val ,negVal);
+
+// basic operations are ( +,-,*,**,/,% )
+
+let str1= 'Hello'
+let str2= ' Javascript'
+console.log(str1+str2);
+// console.log(str1-str2);
+
+// js automatically convert datatypes if necessary 
+// console.log(1+'2');
+// console.log('1'+2);
+// console.log('1'+2+2);
+// console.log(1+1+'2');
+// console.log(true);
+// console.log(+true);
+
+// =======INCREMENT AND DECREMENT =============
+
+let a= 3
+let b= a++
+console.log('a:'+a,'b:'+b);
+
+let x=3
+let y= ++x
+console.log('x:'+x+'y:'+y);
