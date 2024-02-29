4c317c8139174e3fea5e6d86b407ac34fa5d4fe1
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-02-29 20:36:01 +0530

    Update 01_array.js

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,28 +1,28 @@
 const myArr=[1,2,3,4,5,6]
-// console.log(myArr);
+console.log(myArr);
 
-// const myArr2= new Array('A','B','C','D','E')
-// console.log(myArr2);
-// console.log(myArr2[3]);
+const myArr2= new Array('A','B','C','D','E')
+console.log(myArr2);
+console.log(myArr2[3]);
 
-// myArr.push(4)   //to add an element at the end of elements
-// myArr.unshift(4)   //to add an element at the start of elements
-// myArr.shift()       // to remove first element of an array
-// console.log(myArr);
+myArr.push(4)   //to add an element at the end of elements
+myArr.unshift(4)   //to add an element at the start of elements
+myArr.shift()       // to remove first element of an array
+console.log(myArr);
 
-// myArr2.pop()        // to remove last elemtn of an array
-// console.log(myArr2);
-
-
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(9));
+myArr2.pop()        // to remove last elemtn of an array
+console.log(myArr2);
 
 
-// const newArr= myArr.join()
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
 
-// console.log(myArr);
-// console.log(newArr);
-// console.log(typeof newArr);
+
+const newArr= myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
 
 const anotherArr= myArr.slice(1,3)
 console.log(anotherArr);
