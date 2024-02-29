4f36e4d0d6bdd424e16a8da4a6e414edc8e672f4
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-02-29 08:28:37 +0000

    added array file

---
+++ 02_basic/01_array.js
@@ -0,0 +1,26 @@
+const myArr=[1,2,3,4,5,6]
+// console.log(myArr);
+
+// const myArr2= new Array('A','B','C','D','E')
+// console.log(myArr2);
+// console.log(myArr2[3]);
+
+// myArr.push(4)   //to add an element at the end of elements
+// myArr.unshift(4)   //to add an element at the start of elements
+// myArr.shift()       // to remove first element of an array
+// console.log(myArr);
+
+// myArr2.pop()        // to remove last elemtn of an array
+// console.log(myArr2);
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+
+// const newArr= myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
