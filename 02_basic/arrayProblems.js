d7afa9387304288f0b332969804c0b28cbe7e116
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-03-01 09:09:41 +0000

    array problems

---
+++ 02_basic/arrayProblems.js
@@ -0,0 +1,38 @@
+// // declaring array
+// let classmates = ['Aman','Abhay','Adarsh','vihaan']
+// console.log(classmates);
+
+// // declaring array using new keyword
+// let languages= ['HTML','CSS','JAVASCRIPT']
+// console.log(languages);
+
+// check wheather an data is an array or not
+// let dogName = ['rocky','sheru','blecky']
+// console.log(Array.isArray(dogName));
+
+// // getting a specific element of an array
+// const carParts = ['tyre','stering','engine','seat']
+// console.log(carParts[0]);
+// console.log(carParts[1]);
+// console.log(carParts[2]);
+// console.log(carParts[3]);
+
+// // get the last element of an array
+// const letters = ['A','B','C','D','E','F']
+// console.log(letters.length);
+// console.log(letters.length-1);  //last index
+// console.log(letters[letters.length-1]);
+
+// // array to join all array elements in a string
+// const animals = ['dog','cat','lion','tiger']
+// // console.log(animals.join());
+// // console.log(typeof animals.join());
+// const animalStr = animals.join()
+// console.log(animalStr);
+// console.log(typeof animalStr);
+
+// // sort elements of an array
+// const bag = ['books','laptop','pencil Box','bottle']
+// const num = [2,3,5,7,9,5,1,5,3,]
+// console.log(num.sort());
+// console.log(bag.sort());
