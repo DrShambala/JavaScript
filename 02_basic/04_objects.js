8c3ad8aab8eeb8f7aa732cc500325e4ba243b334
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-03-02 14:36:50 +0000

    add obj file

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,21 @@
+const myObj = new Object()    // this is a singleton object
+
+const yourObj  = {}         // this is a non singleton object
+
+yourObj.id = 'name1212'
+yourObj.name  = 'name'
+yourObj.isLoggedIn = true
+
+// console.log(yourObj);
+
+const obj1 = {1:'A',2:'B'}
+const obj2 = {4:'A',3:'B'}
+const obj3 = {5:'A',6:'B'}
+
+// methods of combining 2 or more objects
+
+// const finalObj = {obj1,obj2,obj3}
+// const finalObj = Object.assign(obj1,obj2,obj3)
+// const finalObj = {...obj1,...obj2,...obj3}
+// console.log(finalObj);
+
