c2971a8048943414fd3f994408d4dc0ea7ac8c9b
DrShambala <102889708+DrShambala@users.noreply.github.com>
2024-03-03 15:03:32 +0000

    func file

---
+++ 03_basic/01_function.js
@@ -0,0 +1,22 @@
+function sayMyName() {
+    console.log('A');
+    console.log('D');
+    console.log('A');
+    console.log('R');
+    console.log('S');
+    console.log('H');
+}
+sayMyName()
+
+// function addTwoNum(num1,num2) {
+//     console.log(num1+num2);
+// }
+function addTwoNum(num1,num2) {
+    // console.log(num1+num2);
+    return num1+num2
+}
+
+// addTwoNum()
+// addTwoNum(2,3)
+const result = addTwoNum(2,3)
+// console.log(result);
