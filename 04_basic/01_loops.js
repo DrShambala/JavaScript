c3802a8c063d913c48c69ebbb7d753043b1b3c0c
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-10 16:44:14 +0000

    update loop file

--- 04_basic/01_loops.js
+++ 04_basic/01_loops.js
@@ -30,9 +30,29 @@ myMap.set('dualist','rage')
 // }
 
 for (const [type, agent] of myMap) {
-    console.log(`${agent} is a ${type} type agent`);
+    // console.log(`${agent} is a ${type} type agent`);
     
 }
 
 
 // =========================== for of doesn't work properly for objects ======================
+// ++++++++++++++++++++++++++++loops for objects ++++++++++++++++++++++++++++++
+
+const myObj = {
+    py : 'python',
+    rb : 'ruby',
+    js: 'javascript',
+    css: 'C S S'
+}
+
+// for (const members of myObj) {
+//     console.log(members);
+// }
+
+// this doesn't work
+
+for (const ext in myObj) {
+    // console.log(members);
+    const language = myObj[ext]
+    console.log(`${ext} is use for ${language}`);
+}
