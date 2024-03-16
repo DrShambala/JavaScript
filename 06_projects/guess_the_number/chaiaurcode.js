f6f4b89af36dbde8e3166af4b306fd486e32416f
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-16 16:57:08 +0000

    guess the num

--- 06_projects/guess_the_number/chaiaurcode.js
+++ 06_projects/guess_the_number/chaiaurcode.js
@@ -1,11 +1,24 @@
 // document.addEventListener('click')
 const randomNum = parseInt((Math.random())*100+1)
+let inputValue = document.querySelector('#guessField').value
+const sbmtBtn = document.querySelector('#subt')
+const prevGuesses = document.querySelector('.guesses')
+const remainingGuss = (document.querySelector('.lastResult')).innerHTML
+const lowOrHi = document.querySelector('.lowOrHi')
 
-const inputValue = document.querySelector('#guessField')
-const remainingGuss = document.querySelector('.lastResult')
+const previousGuess = []
+const playGame = true
 
-if (inputValue == randomNum) {
-    
-} else {
+if(playGame){
+    sbmtBtn.addEventListener('click', function(EvObj){
+        EvObj.preventDefault()
+        inputValue = parseInt(inputValue)
+        guessValidation(inputValue)
+    })
+}
 
+function guessValidation(guess) {
+    if (guess < 0 || guess > 500 || isNaN || guess == '') {
+        
+    }
 }
