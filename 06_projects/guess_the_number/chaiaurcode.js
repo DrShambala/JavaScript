814b16bcaf604a578a04bd7748b1c98a78045a6e
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-16 10:41:32 +0000

    added gus the num file

---
+++ 06_projects/guess_the_number/chaiaurcode.js
@@ -0,0 +1,11 @@
+// document.addEventListener('click')
+const randomNum = parseInt((Math.random())*100+1)
+
+const inputValue = document.querySelector('#guessField')
+const remainingGuss = document.querySelector('.lastResult')
+
+if (inputValue == randomNum) {
+    
+} else {
+
+}
