7050b564397aa2a2de4d122018619523f51ef46a
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-16 09:50:22 +0000

    update bmi calc

--- 06_projects/BMI_Calculator/chaiaurcode.js
+++ 06_projects/BMI_Calculator/chaiaurcode.js
@@ -6,6 +6,8 @@ form.addEventListener('submit',function (eObj){
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#results')
+    const stmt = document.querySelector('#stmt')
+    
 
     if (height == ''|| height < 0|| isNaN(height)) {
         result.innerHTML = 'Please enter a valid height'
@@ -14,6 +16,15 @@ form.addEventListener('submit',function (eObj){
         result.innerHTML = 'Please enter a valid weight'
     }
     else{
-        result.textContent = (height*height)/10000
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        result.textContent = bmi
+
+        if (bmi < 18.6) {
+            stmt.textContent = 'Underweight'
+        } else if(bmi > 18.6 && bmi < 24.9) {
+            stmt.textContent = 'Normal'
+        } else{
+            stmt.textContent = 'Overweight'
+        }
     }
 })
