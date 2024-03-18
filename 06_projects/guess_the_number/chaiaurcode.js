85a4ee47c4e92eacfdf28b444ff79fe0d50af6c5
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-18 17:33:25 +0000

    game file

--- 06_projects/guess_the_number/chaiaurcode.js
+++ 06_projects/guess_the_number/chaiaurcode.js
@@ -1,9 +1,9 @@
 // document.addEventListener('click')
-const randomNum = parseInt((Math.random())*100+1)
-let inputValue = document.querySelector('#guessField').value
+const randomNum = parseInt((Math.random())*10+1)
+const inputValue = document.querySelector('#guessField')
 const sbmtBtn = document.querySelector('#subt')
 const prevGuesses = document.querySelector('.guesses')
-const remainingGuss = (document.querySelector('.lastResult')).innerHTML
+const remainingGuss = document.querySelector('.lastResult')
 const lowOrHi = document.querySelector('.lowOrHi')
 
 const previousGuess = []
@@ -12,13 +12,32 @@ const playGame = true
 if(playGame){
     sbmtBtn.addEventListener('click', function(EvObj){
         EvObj.preventDefault()
-        inputValue = parseInt(inputValue)
-        guessValidation(inputValue)
+        const inputValueInt = parseInt(inputValue.value)
+        guessValidation(inputValueInt)
+        // console.log(typeof inputValueInt,inputValueInt);
     })
 }
 
 function guessValidation(guess) {
-    if (guess < 0 || guess > 500 || isNaN || guess == '') {
-        
+    // console.log(typeof guess);
+    if (guess <= 0 || guess >= 10 ) {
+        lowOrHi.textContent = 'enter number between 0 to 10'
+    }else if(guess == NaN || guess === ''){
+        lowOrHi.textContent = 'enter a valid number'
+        // console.log('enter a valid number');
+    } 
+    else{
+        console.log('sdfghjk');
+        // checkNumber(guess)
+    }    
+}
+
+function checkNumber(num){
+    if (num != randomNum) {
+        const remainingGusses = remainingGuss.innerHTML -1
+        console.log(`${remainingGusses} and random num is ${randomNum}`);
+        remainingGuss.innerHTML = remainingGusses
+    } else {
+        console.log('correct!');
     }
 }
