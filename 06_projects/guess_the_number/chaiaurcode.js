36ce5169ff02f84fb71aac591c46195723ab8738
Adarsh <102889708+DrShambala@users.noreply.github.com>
2024-03-20 11:23:40 +0000

    update project file

--- 06_projects/guess_the_number/chaiaurcode.js
+++ 06_projects/guess_the_number/chaiaurcode.js
@@ -2,7 +2,7 @@
 const randomNum = parseInt((Math.random())*10+1)
 const inputValue = document.querySelector('#guessField')
 const sbmtBtn = document.querySelector('#subt')
-const prevGuesses = document.querySelector('.guesses')
+const prevGuesses = document.querySelector('.guesses').innerHTML
 const remainingGuss = document.querySelector('.lastResult')
 const lowOrHi = document.querySelector('.lowOrHi')
 
@@ -12,32 +12,46 @@ const playGame = true
 if(playGame){
     sbmtBtn.addEventListener('click', function(EvObj){
         EvObj.preventDefault()
-        const inputValueInt = parseInt(inputValue.value)
-        guessValidation(inputValueInt)
+        const guess = parseInt(inputValue.value)
+        guessValidation(guess)
         // console.log(typeof inputValueInt,inputValueInt);
     })
 }
 
 function guessValidation(guess) {
-    // console.log(typeof guess);
-    if (guess <= 0 || guess >= 10 ) {
-        lowOrHi.textContent = 'enter number between 0 to 10'
-    }else if(guess == NaN || guess === ''){
-        lowOrHi.textContent = 'enter a valid number'
-        // console.log('enter a valid number');
-    } 
-    else{
-        console.log('sdfghjk');
-        // checkNumber(guess)
-    }    
+        if (isNaN(guess)) {
+            displayMessage('enter a valid number')
+            // alert('guess')
+        }
+        else if(guess < 0 || guess > 11){
+            displayMessage('enter number between 0 to 10')
+            // alert('guess')
+        }
+        else{
+            checkNumber(guess)
+        }
+
 }
+
+function displayMessage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
+  }
 
 function checkNumber(num){
     if (num != randomNum) {
         const remainingGusses = remainingGuss.innerHTML -1
-        console.log(`${remainingGusses} and random num is ${randomNum}`);
         remainingGuss.innerHTML = remainingGusses
+        console.log(`${remainingGusses} and random num is ${randomNum}`);
+        previousGuess.push(num)    
+        prevGuesses.innerHTML = 'akkad bakkad'    
+        if(num < randomNum){
+            displayMessage('your number is Too low ')
+        }else{
+            displayMessage('your number is too high')
+        }
+        inputValue.value = ''
     } else {
         console.log('correct!');
+        displayMessage('CORRECT! ')
     }
 }
